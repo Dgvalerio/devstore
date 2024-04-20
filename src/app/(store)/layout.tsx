e3016d8d81e88a58df7a087c9b793ade88db817b
davigvalerio <al.davigvalerio@gmail.com>
2024-04-20 12:48:47 -0300

    feat(header): create header

--- src/app/(store)/layout.tsx
+++ src/app/(store)/layout.tsx
@@ -5,10 +5,10 @@ import { NextPage } from 'next';
 import { Header } from '@/components/header';
 
 const StoreLayout: NextPage<PropsWithChildren> = ({ children }) => (
-  <>
+  <div className="mx-auto grid min-h-screen w-full max-w-[1600px] grid-rows-app gap-5 p-8">
     <Header />
     {children}
-  </>
+  </div>
 );
 
 export default StoreLayout;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export const metadata: Metadata = {
 
 const RootLayout: NextPage<PropsWithChildren> = ({ children }) => (
   <html lang="pt-br" className={inter.variable}>
-    <body>{children}</body>
+    <body className="bg-zinc-950 text-zinc-50 antialiased">{children}</body>
   </html>
 );
 
