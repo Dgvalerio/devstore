a990118eb3adddd155a3befb7b98426b33f865e1
davigvalerio <al.davigvalerio@gmail.com>
2024-04-27 23:40:17 -0300

    fix(images-config): use remotePatterns instead domains

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: { domains: ['github.com'] },
+  images: {
+    remotePatterns: [
+      { hostname: 'github.com' },
+    ],
+  },
 };
 
 export default nextConfig;
