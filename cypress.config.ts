ee6dea217cd4eb217e2df3ee95f70b0d902c48eb
davigvalerio <al.davigvalerio@gmail.com>
2024-04-28 01:29:47 -0300

    feat(cypress): add cypress to project

---
+++ cypress.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'cypress';
+
+// eslint-disable-next-line no-restricted-syntax
+export default defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
